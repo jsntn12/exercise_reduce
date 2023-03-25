f8b5823cd297178e7f5288d92326453c9e998bd9
jsntn12 <jsantana@nperts-mbp.mynetworksettings.com>
2023-03-25 12:35:10 -0400

    feat: allows users to add a value key pair to a data object.

--- reduce.js
+++ reduce.js
@@ -34,7 +34,6 @@ function vowelCount(str) {
         else if (vowels.includes(char)) {
             vowelCount[char] = 1
         }
-        console.log(vowelCount)
         return vowelCount
     }, {})
 }
@@ -54,7 +53,12 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function(data, next, i) {
+        data[i][key] = value;
+        return data
+    }, arr)
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
